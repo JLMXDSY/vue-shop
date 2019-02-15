b081d36c0ddd1479a108a0c9285f260c718b9cbb
王晶 <wangjing@wangjingdeMacBook-Pro.local>
2019-02-15 17:34:55 +0800

    用户登录功能实现完毕

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入global.css全局样式文件
+import './assets/css/global.css'
+// 引入字体图标样式
+import './assets/fonts/iconfont.css'
+// 引入element组件模块
+import ElementUI from 'element-ui'
+
+// 引入axios并配置
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 把axios配置给vue的$http成员
+Vue.prototype.$http = axios
+
+// 注册element组件给vue
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
